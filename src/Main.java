fef9f92d5450823cb3744692c81fa8ce128ce415
zhenghong <zwang2654@wisc.edu>
2023-03-07 14:29:52 -0600

    git initialize

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * ClassName: ${NAME} Package Description Author zwang2654
+ *
+ * @Create ${DATE} ${TIME}
+ * @Version 1.0
+ */
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
